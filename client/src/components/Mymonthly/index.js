cd3fa51169cc238a994e3da4d946fc6b02ea8ee3
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-12-11 22:57:39 -0500

    added information to my monthly

---
+++ client/src/components/Mymonthly/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function MonthlyOverview() {
+  return (
+    <div>
+      <h2> My Monthly</h2>
+      <p> No monthly parking information available </p>
+      <button> Add monthly </button>
+    </div>
+  );
+}
