7fe9e439d55d6651ae72c0bf371b39b5ab5a53af
sinuhe Montero <sinuhem23@gmail.com>
2019-12-12 21:38:20 -0500

    fixed footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,40 +1,40 @@
-import React from "react";
+import React from 'react';
 // CSS Footer
-import style from "./footerstyle.css";
+import style from './footerstyle.css';
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">
+    <footer className='footer sticky'>
+      <div className='navbar navbar-expand-lg navbar-dark bg-dark'>
+        <a className='navbar-brand' href='/'>
           Logo
         </a>
         <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className='navbar-toggler'
+          type='button'
+          data-toggle='collapse'
+          data-target='#navbarSupportedContent'
+          aria-controls='navbarSupportedContent'
+          aria-expanded='false'
+          aria-label='Toggle navigation'
         >
-          <span className="navbar-toggler-icon"></span>
+          <span className='navbar-toggler-icon'></span>
         </button>
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="/contact">
+        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
+          <ul className='navbar-nav mr-auto'>
+            <li className='nav-item'>
+              <a className='nav-link' href='/contact'>
                 Contact
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/monthly">
+            <li className='nav-item'>
+              <a className='nav-link' href='/monthly'>
                 Monthly
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/aboutus">
+            <li className='nav-item'>
+              <a className='nav-link' href='/aboutus'>
                 About Us
               </a>
             </li>
