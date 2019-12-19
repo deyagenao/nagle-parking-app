c90f3d24af4baba4f52be1875a8aabf9216ec6b7
Rafael De Leon <rafael_de_leon88@hotmail.com>
2019-12-19 02:01:43 -0500

    added image to section about us

--- client/src/components/Aboutus/index.js
+++ client/src/components/Aboutus/index.js
@@ -14,30 +14,36 @@ export default function AboutUs() {
           </p>
         </div>
       </div>
-      <div className='container pushAboutInfoDown'>
-        <div className='row'>
-          <div className='col-lg-8 aboutText'>
-            <h3 className='naglePText'>This Is Nagle Parking</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </p>
-          </div>
+      <section className='carimage'>
+        <div className='container pushAboutInfoDown'>
+          <div className='row'>
+            <div className='col-lg-8 aboutText'>
+              <h3 className='naglePText'>This Is Nagle Parking</h3>
+              <div className='row section-two-control'>
+                <p className='col-sm-10'>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Voluptate aliquid illo voluptatem corporis provident! Dolorum
+                  id at adipisci doloremque amet, aut iusto. Dolorem, corporis
+                  maxime. Atque at minus quis earum?
+                </p>
 
-          <div className='col-lg-4'>
-            <h1 className='followUsAbout text-center'>Follow Us </h1>
-            <SocialMedia />
+                <p className='col-sm-5 section-two-control-2'>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Voluptate aliquid illo voluptatem corporis provident! Dolorum
+                  id at adipisci doloremque amet, aut iusto. Dolorem, corporis
+                  maxime. Atque at minus quis earum?
+                </p>
+                <p className='col-sm-5 section-two-control-2'>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Voluptate aliquid illo voluptatem corporis provident! Dolorum
+                  id at adipisci doloremque amet, aut iusto. Dolorem, corporis
+                  maxime. Atque at minus quis earum?
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
       {/* About Us Points */}
       <div className='container-fluid pushAboutUsPointsDown'>
         <div className='row text-center'>
