51d14630a7124e5d387202a9185d5641c8906e1a
Rafael De Leon <rafael_de_leon88@hotmail.com>
2020-01-05 18:40:51 -0500

    added stripe

---
+++ client/src/components/Payment/index.js
@@ -0,0 +1,16 @@
+import react from 'react';
+import React from 'react';
+
+import { StripeProvider } from 'react-stripe-elements';
+
+import MyStoreCheckout from '../MyStoreCheckout';
+
+export default function Payment() {
+  return (
+    <div>
+      <StripeProvider apiKey='pk_test_nwe2UnrG9Pqx3EBWjbCdHsqP005GI8T6Pl'>
+        <MyStoreCheckout />
+      </StripeProvider>
+    </div>
+  );
+}
