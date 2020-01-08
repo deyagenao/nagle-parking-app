bfcda90bf9c701bfae8076d9d90c35bb058367eb
sinuhe Montero <sinuhem23@gmail.com>
2020-01-08 14:38:24 -0500

    got sign in mobile friendly for RON'

--- client/src/components/AdminSideBar/index.js
+++ client/src/components/AdminSideBar/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-function AdminSideBar (props){
-    return (
-        <div>
-            <ul className="nav flex-column col-lg-3">
-                <li className="nav-item">
-                    <button className="nav-link text-dark" onClick={props.getAllCustomers}>View All Customers</button>
-                </li>
-                <li className="nav-item">
-                    <button className="nav-link text-dark" onClick={props.getAllMonthly}>View All Monthly Customers</button>
-                </li>
-                <li className="nav-item">
-                    <button className="nav-link text-dark" onClick={props.getAllPickUps}>View All Scheduled Pick Ups</button>
-                </li>
-            </ul>
-        </div>
-    )
-};
+function AdminSideBar(props) {
+  return (
+    <div>
+      <ul className='nav flex-column col-lg-3'>
+        <li className='nav-item'>
+          <button
+            className='nav-link text-dark'
+            onClick={props.getAllCustomers}
+          >
+            View All Customers
+          </button>
+        </li>
+        <li className='nav-item'>
+          <button className='nav-link text-dark' onClick={props.getAllMonthly}>
+            View All Monthly Customers
+          </button>
+        </li>
+        <li className='nav-item'>
+          <button className='nav-link text-dark' onClick={props.getAllPickUps}>
+            View All Scheduled Pick Ups
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 export default AdminSideBar;
