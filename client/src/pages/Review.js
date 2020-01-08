16fd0285dda0d36c1cd9004d73d0abff7d3e6a75
Rafael De Leon <rafael_de_leon88@hotmail.com>
2020-01-08 01:31:06 -0500

    added style to container for my account and review

--- client/src/pages/Review.js
+++ client/src/pages/Review.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReviewForm from '../components/ReviewForm';
+import Subnav from '../components/Subnav';
 
 export default function ReviewPage() {
   return (
     <div>
+      <Subnav />
       <ReviewForm />
     </div>
   );
