9c4bc55aa565b8a26daf63d3fd4d26dcb3be186c
sinuhe Montero <sinuhem23@gmail.com>
2020-01-10 20:46:33 -0500

    made monthly mobile

--- client/src/components/Mymonthlyinfo/index.js
+++ client/src/components/Mymonthlyinfo/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import API from '../../utils/API';
 import './monthlyinfo.css';
 import ReviewLink from '../ReviewLink';
+import { Link } from 'react-router-dom';
 
 import { Row, Container } from '../Grid';
 
@@ -38,43 +39,69 @@ class MonthlyInfo extends Component {
               {this.state.userData.lastName}
             </h2>
           </Row>
+
           <Container className='tableCon'>
             <Row>
-              <table className='table table-hover '>
-                <tbody>
-                  <tr>
-                    <th scope='row'>Car Type:</th>
-                    <td>{this.state.userData.carType}</td>
-                  </tr>
-                  <tr>
-                    <th scope='row'> Make:</th>
-                    <td>{this.state.userData.carMake}</td>
-                  </tr>
-                  <tr>
-                    <th scope='row'> Model:</th>
-                    <td>{this.state.userData.carModel}</td>
-                  </tr>
-                  <tr>
-                    <th scope='row'> Color:</th>
-                    <td>{this.state.userData.carColor}</td>
-                  </tr>
-                  <tr>
-                    <th scope='row'>License Plate:</th>
-                    <td>{this.state.userData.licensePlate}</td>
-                  </tr>
+              <div className='col-md-6'>
+                <table className='table table-hover  tableControl'>
+                  <tbody>
+                    <tr>
+                      <th scope='row'>Car Type:</th>
+                      <td>{this.state.userData.carType}</td>
+                    </tr>
+                    <tr>
+                      <th scope='row'> Make:</th>
+                      <td>{this.state.userData.carMake}</td>
+                    </tr>
+                    <tr>
+                      <th scope='row'> Model:</th>
+                      <td>{this.state.userData.carModel}</td>
+                    </tr>
+                    <tr>
+                      <th scope='row'> Color:</th>
+                      <td>{this.state.userData.carColor}</td>
+                    </tr>
+                    <tr>
+                      <th scope='row'>License Plate:</th>
+                      <td>{this.state.userData.licensePlate}</td>
+                    </tr>
 
-                  {/* <tr>
+                    {/* <tr>
                 {this.state.userData.paid ? <td>Paid!</td> : <td>Not paid!</td>}
               </tr> */}
-                </tbody>
-              </table>
+                  </tbody>
+                </table>
+                <div className=''>
+                  {/* reviews */}
+                  <ReviewLink />
+                </div>
+              </div>
+              <div className='col-md-6'>
+                <div class='card cardmonthlyInfo'>
+                  <img
+                    src='https://rjma.com/wp-content/uploads/2019/10/img-team1.png'
+                    class='card-img-top'
+                    alt='...'
+                  ></img>
+                  <div class='card-body text-center'>
+                    <h5 class='card-title '>Enjoy the Perks!</h5>
+                    <p class='card-text'>
+                      Want to set a pick up for for vechile to be ready before
+                      arrival?
+                    </p>
+                    <div className='text-center'>
+                      <Link
+                        className='btn btn-warning setPickUpBTN '
+                        to='/mypickup'
+                      >
+                        Set Pick Up
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </Row>
           </Container>
-
-          <div className='col-md-12'>
-            {/* reviews */}
-            <ReviewLink />
-          </div>
 
           <Row>
             <div className='secondconforTextImg'>
